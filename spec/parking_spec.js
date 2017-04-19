33eb47f8599f3ee4163160976c63bca64e27be63
MrPickles2009 <MrPickles2009@users.noreply.github.com>
2017-04-19 12:59:01 -0400

    Add files via upload

---
+++ spec/parking_spec.js
@@ -0,0 +1,8 @@
+let parkingApp = require('../parkingApp');
+
+describe('An app used to determine where anyone can park at James Madison University', () => {
+
+  it('can perform a smoke test', () => {
+    expect(true).toBe(true);
+  });
+});
